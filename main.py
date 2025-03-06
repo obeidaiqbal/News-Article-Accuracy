b4a4392c89207ae0b3ffc7e192460ad973e7a250
Obeida Iqbal <obeidaiqbal41@gmail.com>
2025-03-05 16:32:29 -0800

    Cleaned up main file, added function structure

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+# News Article Misleading Check
+# This script is intended to read through a news article and its title
+# and tell the user whether the article is misleading
+# Author: Obeida Iqbal
+
 from transformers import pipeline
 
 
-classifier = pipeline("sentiment-analysis")
-print(classifier("We are very happy to show you the 🤗 Transformers library."))
+def main():
+    """ Main program """
+    classifier = pipeline("sentiment-analysis")
+    results = classifier(["We are very happy to show you the 🤗 Transformers library.", "I hate driving so much", "I love that I hate you"])
+    for result in results:
+        print(f"label: {result['label']}, with score: {round(result['score'], 4)}")
+
+if __name__ == "__main__":
+    main()
