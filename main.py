d5b7d3fe6a1d7222690cf0057eee77b13172bb32
Obeida Iqbal <obeidaiqbal41@gmail.com>
2025-03-06 12:28:18 -0800

    cleaned up the functions in main.py

--- main.py
+++ main.py
@@ -6,12 +6,17 @@
 from transformers import pipeline
 
 
-def main():
-    """ Main program """
+def check_sentiment(prompts):
+    """ Takes a list of strings as a parameter and prints the sentiment of each string """
     classifier = pipeline("sentiment-analysis")
-    results = classifier(["We are very happy to show you the 🤗 Transformers library.", "I hate driving so much", "I love that I hate you"])
+    results = classifier(prompts)
     for result in results:
         print(f"label: {result['label']}, with score: {round(result['score'], 4)}")
+
+def main():
+    """ Main program """
+    list = ["We are very happy to show you the 🤗 Transformers library.", "I hate driving so much", "I love that I hate you"]
+    check_sentiment(list)
 
 if __name__ == "__main__":
     main()
